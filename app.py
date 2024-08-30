5f26705d1cf3f5c8d243e611283f37b2246ee397
Ppouts <pierre.pouts@gmail.com>
2024-08-30 13:27:05 +0200

    oui

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import os  
-import pyzipper 
-import pikepdf  
 from flask import Flask, render_template, request 
 
 app = Flask(__name__)  
@@ -9,9 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  
 
-@app.route('/Stage B3')
-def Stage_B3():
-    return render_template('Stage B3.html')
+@app.route('/stage_B2')
+def stage_B2():
+    return render_template('stage_B2.html')
+
+@app.route('/cahier_des_charges')
+def cahier_des_charges():
+    return render_template('cahier_des_charges.html')
+
+@app.route('/a_propos')
+def a_propos():
+    return render_template('a_propos.html')
 
 
 
